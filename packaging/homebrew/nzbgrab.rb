4d996f36d4c846faa15618325e8fd073cc895662
Andy Jeffries <andy@andyjeffries.co.uk>
2026-05-15 07:59:48 +0100

    Fix CI branch name and update Homebrew formula

--- packaging/homebrew/nzbgrab.rb
+++ packaging/homebrew/nzbgrab.rb
@@ -2,9 +2,9 @@ class Nzbgrab < Formula
   desc "Fast parallel NZB downloader with PAR2 and extraction"
   homepage "https://github.com/andyjeffries/nzbgrab"
   url "https://github.com/andyjeffries/nzbgrab/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "cd5eca02985b89a40d1b81b372c22ade3d6530897fbce093f349d53ff528a435"
   license "MIT"
-  head "https://github.com/andyjeffries/nzbgrab.git", branch: "main"
+  head "https://github.com/andyjeffries/nzbgrab.git", branch: "master"
 
   depends_on "go" => :build
   depends_on "par2" => :recommended
